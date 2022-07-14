c2c898bac8a8170580890e6024f8f9c426041427
Krishna <kmuthukr@buffalo.edu>
2022-07-14 00:15:28 -0400

    resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Krishnakumar_Muthukrishnan.pdf";
+import pdf from "../../Assets/Krishnakumar_Muthukrishnan.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -10,8 +10,6 @@ import Footer from "../Footer";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-//const resumeLink =
-//  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
 const resumeLink="  https://raw.githubusercontent.com/kmuthukr/portfolio/main/src/Assets/Krishnakumar_Muthukrishnan.pdf";
 
 function ResumeNew() {
@@ -44,10 +42,11 @@ function ResumeNew() {
           <Document file={resumeLink} className="d-flex justify-content-center">
             <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
-          <Document file={resumeLink} className="d-flex justify-content-center">
+    {/** As the number of pages in ur resume increases add those many Document tag here
+           <Document file={resumeLink} className="d-flex justify-content-center">
             <Page pageNumber={3} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
-
+*/}
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
